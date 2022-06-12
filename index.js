d53dfebf588c0493f20d22922f1fa3687ddef0f2
kevin breid <kevinbreid@yahoo.com>
2022-06-11 18:42:39 -0700

    commit tree c360f2c1a3d71b2b4925c9dc836fbcf87f47781a

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solution in this file!
+let drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+
+
+const returnFirstTwoDrivers = function(drivers){
+
+    return drivers.slice(0,2)
+}
+
+const returnLastTwoDrivers = function(drivers){
+
+    return drivers.slice(-2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(int){
+
+    return function multiply(fare){
+
+        return fare * int
+    }
+
+
+}
+
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler =  createFareMultiplier(3)
+
+const fareQuintupler = createFareMultiplier(5)
+
+const selectDifferentDrivers = (drivers, func) => func(drivers);
